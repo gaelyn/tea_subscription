87c6140d9b4d759bbc74155ba718d489b7a96e6c
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-02 16:08:21 -0500

    Add validations

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,4 +1,6 @@
 class Tea < ApplicationRecord
   has_many :subscriptions
   has_many :customers, through: :subscriptions
+
+  validates_presence_of :name, :description, :brew_time, :temperature
 end
