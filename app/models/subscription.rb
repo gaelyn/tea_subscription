34af1e714f3dca9b7590907b8ef4805a3109d915
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-02 16:07:28 -0500

    Add validations

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,6 +2,9 @@ class Subscription < ApplicationRecord
   belongs_to :tea
   belongs_to :customer
 
+  validates_presence_of :tea_id, :customer_id, :title, :price, :status, :frequency
+
   enum status: [:active, :inactive]
   enum frequency: [:monthly, :bimonthly, :trimonthly]
+
 end
