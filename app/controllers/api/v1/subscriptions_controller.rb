274619ce7da4a2bb9d596c19746674c28449292b
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-02 17:18:05 -0500

    Render Serialized JSON

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::SubscriptionsController < ApplicationController
   def create
     sub = Subscription.new(subscription_params)
     if sub.save
-      # render json: SubscriptionSerializer.new(sub), status: 201
-      render json: sub, status: 201
+      render json: SubscriptionSerializer.new(sub), status: 201
     else
       render json: { errors: 'Unable to create subscription' }, status: :bad_request
     end
