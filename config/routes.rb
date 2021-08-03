71dc2a71e95bb5f38c1f8a6241af0df31364c2fc
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-03 11:55:23 -0500

    Add route for customer subs

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :subscriptions
+      resources :customers do
+        resources :subscriptions, only: :index
+      end
     end
   end
 end
