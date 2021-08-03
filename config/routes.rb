2fd2aeb392075aed2b0c0866a950ff7719d3b90e
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-03 14:20:15 -0500

    Create route for update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :subscriptions
       resources :customers do
-        resources :subscriptions, only: :index
+        resources :subscriptions, only: [:index, :update]
       end
     end
   end
