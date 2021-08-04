68f947b5c28a33753af92b2effa96ceb4e8732f9
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-04 09:12:54 -0500

    Change routes to handrolled for readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :subscriptions
-      resources :customers do
-        resources :subscriptions, only: [:index, :update]
-      end
+      get '/customers/:customer_id/subscriptions', to: 'customers/subscriptions#index'
+      post '/customers/:customer_id/subscriptions', to: 'customers/subscriptions#create'
+      patch '/customers/:customer_id/subscriptions/:id', to: 'customers/subscriptions#update'
     end
   end
 end
