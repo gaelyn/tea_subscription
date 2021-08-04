bcdc75c9751299928d9f0c521a03ce04e4592110
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-04 09:12:17 -0500

    Change controller file structure

--- app/controllers/api/v1/customers/subscriptions_controller.rb
+++ app/controllers/api/v1/customers/subscriptions_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::SubscriptionsController < ApplicationController
+class Api::V1::Customers::SubscriptionsController < ApplicationController
   def index
     customer = Customer.find(params[:customer_id])
     render json: SubscriptionSerializer.new(customer.subscriptions)
