3ed254e98faacef2a4396ee7de35faac17214c85
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-05 21:06:15 -0500

    Abstact away errors from create action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::API
   rescue_from ArgumentError, with: :invalid_params
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
+  def no_subs
+    render json: { errors: "No subscriptions found" }, status: :ok
+  end
 
   private
 
   def invalid_params(e)
     render json: { errors: e }, status: :bad_request
+  end
+
+  def invalid_record(e)
+    render json: { errors: e.record.errors.full_messages.to_sentence }, status: :bad_request
   end
 end
