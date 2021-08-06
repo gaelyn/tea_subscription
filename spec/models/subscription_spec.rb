25e6745183cbcc411edc3bdb471d4164da111948
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-06 08:43:01 -0500

    Remove unused code from model tests

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -14,18 +14,4 @@ RSpec.describe Subscription, type: :model do
     it { should validate_presence_of(:tea_id) }
     it { should validate_presence_of(:customer_id) }
   end
-
-  before :each do
-
-  end
-
-  describe 'class methods' do
-    describe '#' do
-    end
-  end
-
-  describe 'instance methods' do
-    describe '.' do
-    end
-  end
 end
